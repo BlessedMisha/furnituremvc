dc8d1edc1d0c2c2fac2fdee61be4b6b85b1ea8ac
unbelievable <124208377+BlessedMisha@users.noreply.github.com>
2024-05-13 23:11:42 +0300

    tried do shopcart page
    idk,now its not working

---
+++ FurnitureShoppingCartMvc/FurnitureShoppingCartMvcUi/Controllers/CatalogItemController.cs
@@ -0,0 +1,37 @@
+﻿using FurnitureShoppingCartMvcUi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureShoppingCartMvcUi.Controllers
+{
+    public class CatalogItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CatalogItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var catalogItems = _context.CatalogItems.ToList();
+            var catalogItemModels = catalogItems.Select(item => item.Transform()).ToList();
+            return View(catalogItemModels);
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(int id)
+        {
+            var catalogItem = _context.CatalogItems.Find(id);
+            if (catalogItem == null)
+            {
+                return NotFound();
+            }
+
+            // Додавання товару до корзини
+            // Це може відрізнятися в залежності від вашої реалізації корзини
+
+            return RedirectToAction("Index", "Cart"); // Перенаправте користувача на сторінку корзини
+        }
+    }
+}
